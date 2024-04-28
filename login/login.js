b596a6c69220d9d14682915aee09f679cd6b9305
Vo Thanh Trang <tiki123trang@gmail.com>
2024-04-29 01:09:21 +0700

    files upload

---
+++ login/login.js
@@ -0,0 +1,40 @@
+/*===========
+EMAIL VALIDATION
+==============*/
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+thongbao();
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("Địa chỉ Email không hợp lệ!");
+document.form1.text1.focus();
+return false;
+}
+}
+
+
+/*===========
+EMAIL VALIDATION
+==============*/
+function thongbao(){
+    var email = document.getElementById("email").value;
+    var mk = document.getElementById("password").value;
+
+    
+    
+    
+    
+
+    confirm(
+        "Bạn đã đăng nhập với Email: "
+    + email 
+    
+        
+    )
+}
